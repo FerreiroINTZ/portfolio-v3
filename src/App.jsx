a6139449f4de4cf3dc5eba39b0aef36588afb3a4
Gabriel Feipe <gabrielferreiro963@gmail.com>
2025-03-26 14:35:11 -0300

    Criado: Titulo, nav barr, sobre mim
    a Nav Barr precisa ser mudada para ser um carrosel

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,22 @@
 function App() {
   return (
     <>
-      <h1>Gabriel</h1>
+      <div>
+        <span className="lights"></span>
+        {/* <span className="lights"></span> */}
+      </div>
+      <h1>
+        <span className="title title1">Ola, seja</span>
+        <span className="title title2">Bem Vindo!</span>
+        <span className="title title3">ao meu Portfolio</span>
+      </h1>
+      <nav>
+        <span>Menu</span>
+        <span>Tecnologias</span>
+        <span>Projetos</span>
+        <span>Contatos</span>
+      </nav>
+      <p>Portfolio de um desenvolvedor Full-Stack em busca de projetos ou vagas. Capaz de criar aplicacoes web com ou sem APIs proprias. No front-end, sendo capaz de criar animacoes e responsividade usando frameworks e conceitos de responsividade. No Back-end capaz de criar sistemas de autenticacao, cookies, Banco de dados, e/ou features expecificas.</p>
     </>
   )
 }
